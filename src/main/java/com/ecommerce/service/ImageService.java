21b881905cb895b23ddbd9c65daf83cbcb9c8e52
shad <=>
2016-09-10 15:47:09 +0300

    Add ImageService - interface that provides methods for dynamic images stored on fs. Add ImageServiceImpl - implementation that provides caching and delegates to ImageUtil.
    Add ImageUtil - utility that handles images on filesystem.

---
+++ src/main/java/com/ecommerce/service/ImageService.java
@@ -0,0 +1,34 @@
+package com.ecommerce.service;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 
+ * Service Layer interface providing
+ * basic save and retrieve operations
+ * for images.
+ * @throws IOExceptions when missing files
+ * or lacking credentials to save in directories 
+ */
+public interface ImageService {
+
+	/**
+	 * Retrieve an image as a base 64 string
+	 * @param path of the file to use
+	 * @return an image converted to a base 64 string
+	 * @throws IOException
+	 */
+	String getB64(String path) throws IOException;
+	
+	byte [] loadImage(String path) throws IOException;
+	
+	/**
+	 * Saves an image to a predefined destination
+	 * @param file
+	 * @param contentType
+	 * @param fileName
+	 * @throws IOException
+	 */
+	String saveImage(File file,String contentType,String fileName) throws IOException;
+}
