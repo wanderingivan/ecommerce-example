47d5e6b8b0b6900407356815816ff845b0752fc0
wanderingivan <angushev.ivan@gmail.com>
2016-10-01 12:24:15 +0300

    Add IncorrectPasswordException class.

---
+++ src/main/java/com/ecommerce/service/impl/IncorrectPasswordException.java
@@ -0,0 +1,10 @@
+package com.ecommerce.service.impl;
+
+public class IncorrectPasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3786074376754301334L;
+
+}
