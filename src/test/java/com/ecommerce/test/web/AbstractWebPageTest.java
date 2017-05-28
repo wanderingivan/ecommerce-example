f77de94d069da52da9d5a300d25523e32656cd7b
wanderingivan <angushev.ivan@gmail.com>
2017-05-28 19:08:04 +0300

    Add javadoc to AbstractWebPageTest

--- src/test/java/com/ecommerce/test/web/AbstractWebPageTest.java
+++ src/test/java/com/ecommerce/test/web/AbstractWebPageTest.java
@@ -6,8 +6,12 @@ import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-
-public class AbstractWebPageTest {
+/**
+ * Abstract class defining common 
+ * dependencies and methods for loading
+ * pages
+ */
+public abstract class AbstractWebPageTest {
 
     @Drone
     protected WebDriver driver;
@@ -17,6 +21,11 @@ public class AbstractWebPageTest {
     
     
     
+    /**
+     * Appends the specified url to the deploymentUrl 
+     * and sets the window to 1920x1080 before attempting to load
+     * @param url the url to load
+     */
     protected void loadPage(String url){
         driver.manage().window().setSize(new Dimension(1920,1080));
         driver.get(deploymentUrl.toString()
